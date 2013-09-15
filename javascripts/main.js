9cfb6acb6b00db1fefa171d9780a0fe2cf1047f2
baivong <baivong@users.noreply.github.com>
2013-09-15 18:03:59 +0700

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,8 @@
 $(function(){
-  $(".tabs a").click(function(){
-    $(".active").removeClass();
-    $(this).parent().addClass("active");
-    $("#box > div").hide();
-    $("#" + this.id + "-box").show();
-  });
+  	$(".tabs a").click(function(){
+    	$(".active").removeClass();
+    	$(this).parent().addClass("active");
+    	$("#comment > div").hide();
+    	$("#" + this.id + "-box").show();
+  	});
 });
